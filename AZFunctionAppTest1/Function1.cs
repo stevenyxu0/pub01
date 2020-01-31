344121db425f1d17b7e190f18cde11f03496ff15
Steven.Xu <stevenyxu@yahoo.com>
2020-01-31 14:11:18 -0500

    This is atest

--- AZFunctionAppTest1/Function1.cs
+++ AZFunctionAppTest1/Function1.cs
@@ -25,6 +25,9 @@ namespace AZFunctionAppTest1
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
+            string strTmp = "";
+            strTmp = name.ToUpper();
+
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
